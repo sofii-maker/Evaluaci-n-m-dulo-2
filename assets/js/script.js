ea17c772fd79e7f856adba7df98eed09b40c6a5e
sofii <sophia.contreraskine@gmail.com>
2025-11-27 17:34:34 -0300

    página principal lista

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+function ofertas() {
+    const el3 = document.getElementById('p3');
+    const el4 = document.getElementById('p4');
+    const boton = document.getElementById('botonoferta');
+
+    if (el3.classList.contains('oculto')) {
+    
+        el3.classList.remove('oculto');
+        el4.classList.remove('oculto');
+        boton.textContent = 'Sale%';
+    } else {
+    
+        el3.classList.add('oculto');
+        el4.classList.add('oculto');
+        boton.textContent = 'Mostrar todos';
+    }
+}
+
+function catalogo() {
+    const catalog = document.getElementById("botonoferta");
+
+    catalog.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     const likeButtons = document.querySelectorAll('.btn-like');
 
@@ -29,3 +56,4 @@ function añadir(){
         contador.textContent = parseInt(valor) + 1
     
 }
+
